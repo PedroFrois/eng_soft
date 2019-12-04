d41b8d8590a02a67eb7b44d24283116d1747f64c
VictorMeyer139 <victormeyer2012@hotmail.com>
2019-12-04 08:19:55 -0300

    Organizando

--- myEnv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\users\milenadelarete\downloads\tp_finales-master\imobiliaria\tp_finales-master\myenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
